b97d84b9fa4b389663dc0ded5959219e406c1a2f
vitor2233 <vitorguilherme2233@gmail.com>
2024-10-23 07:59:08 -0300

    Added aws certification

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,6 +1,7 @@
 import { ArrowSquareOut, Buildings, Envelope, GitCommit } from 'phosphor-react'
 import styles from './Profile.module.css'
 import { useEffect, useState } from 'react';
+import logo from '../assets/cloud-practitioner.png';
 
 interface GithubInformation {
     profileUrl: string;
@@ -36,13 +37,14 @@ export function Profile() {
         <div className={styles.profile}>
             <aside className={styles.profileImage}>
                 <img className={styles.avatar} src={githubInformation.profileUrl} />
+                <img className={styles.cloudBadge} src={logo} alt="AWS Cloud Practitioner Badge" onClick={() => window.open('https://www.credly.com/badges/7f5f1c84-258a-4355-89af-7b62839769e7', '_blank')} />
             </aside>
             <main className={styles.profileInformation}>
                 <div>
                     <h2 className={styles.firstName}>{githubInformation.name}</h2>
                 </div>
                 <div>
-                    <p className={styles.description}>Desenvolvedor backend com mais de 3 anos de experiência em C# e Angular. Minhas habilidades também se estendem para Node.js e Flutter, o que me permite criar soluções robustas e eficientes tanto para web quanto para dispositivos móveis. Sou formado em Sistemas de Informação pela Faculdade UNA e possuo um curso técnico em TI pelo SENAI. Sou apaixonado por resolver desafios complexos e estou constantemente buscando aprender e me aprimorar.</p>
+                    <p className={styles.description}>Desenvolvedor backend com mais de 4 anos de experiência em C# e Angular. Minhas habilidades também se estendem para Node.js e Flutter, o que me permite criar soluções robustas e eficientes tanto para web quanto para dispositivos móveis. Sou formado em Sistemas de Informação pela Faculdade UNA, possuo curso técnico em TI pelo SENAI e também a certificação AWS Cloud Practitioner. Sou apaixonado por resolver desafios complexos e estou constantemente buscando aprender e me aprimorar.</p>
                 </div>
                 <div className={styles.buttons}>
                     <a
